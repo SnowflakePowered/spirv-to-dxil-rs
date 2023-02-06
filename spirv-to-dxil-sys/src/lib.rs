70b57831d3a12ce355816f3463c797b712eac042
chyyran <ronny@ronnychan.ca>
2023-02-06 03:07:48 -0500

    build: try and fix downstream docs.rs

--- spirv-to-dxil-sys/src/lib.rs
+++ spirv-to-dxil-sys/src/lib.rs
@@ -2,7 +2,6 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 
-#[cfg(not(feature = "included-bindings"))]
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
 #[cfg(feature = "included-bindings")]
