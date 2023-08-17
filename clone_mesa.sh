33964f74222c05bccf0bcb66e4deab1a7a88234c
chyyran <ronny@ronnychan.ca>
2023-08-17 02:03:11 -0400

    build: fix sparse-checkout script

--- clone_mesa.sh
+++ clone_mesa.sh
@@ -1,9 +1,8 @@
-git clone --filter=blob:none --no-checkout https://gitlab.freedesktop.org/mesa/mesa
-git submodule add https://gitlab.freedesktop.org/mesa/mesa spirv-to-dxil-sys/native/mesa
-git submodule absorbgitdirs
+git submodule init spirv-to-dxil-sys/native/mesa
+git submodule update --init --filter=blob:none --depth 1 --single-branch --progress spirv-to-dxil-sys/native/mesa
+git submodule absorbgitdirs spirv-to-dxil-sys/native/mesa
 
 git -C spirv-to-dxil-sys/native/mesa config core.sparseCheckout true
 git -C spirv-to-dxil-sys/native/mesa config core.symlinks false
-
-cp ./spirv-to-dxil-sys/native/mesa-sparse-checkout ./.git/modules/spirv-to-dxil-sys/native/mesa/info
-git submodule update --init --force --checkout spirv-to-dxil-sys/native/mesa
+cp spirv-to-dxil-sys/native/mesa-sparse-checkout .git/modules/spirv-to-dxil-sys/native/mesa/info/sparse-checkout
+git submodule foreach git sparse-checkout reapply
