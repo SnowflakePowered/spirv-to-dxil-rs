5ee72f9257a764cb6ce5c00aa8e2e583745485a2
chyyran <ronny@ronnychan.ca>
2023-08-17 02:49:32 -0400

    build: install libdrm on ubuntu

--- clone_mesa.sh
+++ clone_mesa.sh
@@ -1,6 +1,6 @@
 git submodule init spirv-to-dxil-sys/native/mesa
 git submodule update --init --filter=blob:none --depth 1 --single-branch --progress spirv-to-dxil-sys/native/mesa
-git submodule absorbgitdirs spirv-to-dxil-sys/native/mesa
+git submodule absorbgitdirs
 
 git -C spirv-to-dxil-sys/native/mesa config core.sparseCheckout true
 git -C spirv-to-dxil-sys/native/mesa config core.symlinks false
