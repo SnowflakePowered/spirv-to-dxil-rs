e059119064af1678aa47b9b99e2bdc8db89e25eb
chyyran <ronny@ronnychan.ca>
2023-12-09 18:53:39 -0500

    build: use explicit math defines

--- spirv-to-dxil-sys/build.rs
+++ spirv-to-dxil-sys/build.rs
@@ -11,14 +11,29 @@ fn main() {
     let mut build = cc::Build::new();
 
     build
-        .cpp(true)
         .std("c11")
         .define("HAVE_STRUCT_TIMESPEC", None)
         // We still should support Windows 7...
         .define("WINDOWS_NO_FUTEX", None)
-        .define("_USE_MATH_DEFINES", None)
         .define("PACKAGE_VERSION", "\"100\"")
         .define("UTIL_ARCH_LITTLE_ENDIAN", None)
+        .define("__STDC_CONSTANT_MACROS", None)
+        .define("__STDC_FORMAT_MACROS", None)
+        .define("__STDC_LIMIT_MACROS", None)
+        .define("M_E", "2.71828182845904523536")
+        .define("M_LOG2E", "1.44269504088896340736")
+        .define("M_LOG10E", "0.434294481903251827651")
+        .define("M_LN2", "0.693147180559945309417")
+        .define("M_LN10", "2.30258509299404568402")
+        .define("M_PI", "3.14159265358979323846")
+        .define("M_PI_2", "1.57079632679489661923")
+        .define("M_PI_4", "0.785398163397448309616")
+        .define("M_1_PI", "0.318309886183790671538")
+        .define("M_2_PI", "0.636619772367581343076")
+        .define("M_2_SQRTPI", "1.12837916709551257390")
+        .define("M_SQRT2", "1.41421356237309504880")
+        .define("M_SQRT1_2", "0.707106781186547524401")
+        .flag_if_supported("-fpermissive")
         .includes(&[
             "native/mesa/include",
             "native/mesa_mako",
@@ -108,6 +123,15 @@ fn main() {
         "native/mesa/src/util/format"
     ];
 
+    if cfg!(target_endian = "big") {
+        build.define(
+            "UTIL_ARCH_BIG_ENDIAN", "1"
+        ).define("UTIL_ARCH_LITTLE_ENDIAN", "0");
+    } else {
+        build.define(
+            "UTIL_ARCH_BIG_ENDIAN", "0"
+        ).define("UTIL_ARCH_LITTLE_ENDIAN", "1");
+    };
 
     for &path in compile_paths {
         for file in std::fs::read_dir(path).unwrap() {
