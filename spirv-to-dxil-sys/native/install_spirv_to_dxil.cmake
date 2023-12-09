dc14be31f3129d8f8cde606367e36b08d82274fa
chyyran <ronny@ronnychan.ca>
2023-12-09 00:50:14 -0500

    build: don't build libvulkan_util

--- spirv-to-dxil-sys/native/install_spirv_to_dxil.cmake
+++ spirv-to-dxil-sys/native/install_spirv_to_dxil.cmake
@@ -1,7 +1,5 @@
 file(INSTALL ${MESA_BUILD}/src/microsoft/spirv_to_dxil/libspirv_to_dxil.a DESTINATION ${OUT_DIR}/lib)
-file(INSTALL ${MESA_BUILD}/src/vulkan/util/libvulkan_util.a DESTINATION ${OUT_DIR}/lib)
 
 if (WIN32)
-    file(RENAME ${OUT_DIR}/lib/libvulkan_util.a ${OUT_DIR}/lib/vulkan_util.lib)
     file(RENAME ${OUT_DIR}/lib/libspirv_to_dxil.a ${OUT_DIR}/lib/spirv_to_dxil.lib)
 endif()
